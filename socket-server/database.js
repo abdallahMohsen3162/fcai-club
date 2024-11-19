7a868b66931ebb2e1f47c26f5ba31e967058fde8
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2024-11-19 21:00:42 +0200

    Update database.js

--- socket-server/database.js
+++ socket-server/database.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgres://default:h3InKsOHBPX9@ep-fragrant-dream-a4rhmytm-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  connectionString: "",
 });
 
 
